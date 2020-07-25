96659281f3a920d62d8d6686b036bab480f40b06
Sahory31 <bsahory_sandoval@hotmail.com>
2020-07-25 02:32:48 -0500

    Creation of the repository with a basic logIn form. (test)

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/index.css';
+import LogInForm from './views/logInForm';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <LogInForm />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
