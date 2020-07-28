e66888f275d179c5f8717c2ba98c48be3a0de4d8
Sahory31 <bsahory_sandoval@hotmail.com>
2020-07-28 13:38:52 -0500

    Updates

---
+++ precache-manifest.8bc88c484426668a154d5e00f2c0b308.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae85ae766e0c92b4f59c332cf7a0e440",
+    "url": "/Sahory31/LogInChallenge/index.html"
+  },
+  {
+    "revision": "576664669ba2e0ef961d",
+    "url": "/Sahory31/LogInChallenge/static/css/main.7fb84e0c.chunk.css"
+  },
+  {
+    "revision": "53fbea2c33614dcc82d2",
+    "url": "/Sahory31/LogInChallenge/static/js/2.c3f690ae.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/Sahory31/LogInChallenge/static/js/2.c3f690ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "576664669ba2e0ef961d",
+    "url": "/Sahory31/LogInChallenge/static/js/main.3b63ad28.chunk.js"
+  },
+  {
+    "revision": "c659845e931ec43c307a",
+    "url": "/Sahory31/LogInChallenge/static/js/runtime-main.906edaed.js"
+  },
+  {
+    "revision": "f5988b40681ded12210fcd1c02110546",
+    "url": "/Sahory31/LogInChallenge/static/media/computer.f5988b40.png"
+  },
+  {
+    "revision": "4d65b8f7ef728e9eb7373eab20754a9e",
+    "url": "/Sahory31/LogInChallenge/static/media/logoHG.4d65b8f7.png"
+  }
+]);
